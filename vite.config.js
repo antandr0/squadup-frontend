b952e96df1fd4af2fae07afad3f51a18bef8a2f0
Ваше имя <@.com>
2025-10-21 22:57:07 +0300

    fix: radical solution - new entry point and config

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: true,
-    port: 3000,
-  },
   build: {
     outDir: 'dist',
-    sourcemap: true,
+    sourcemap: false
+  },
+  server: {
+    port: 3000,
+    open: true
   }
 })
